cc437fe2ef42834f554dfda5fdc5586343c4fc41
Reflection <reflectionoflines@gmail.com>
2023-01-11 12:47:31 -0600

    Add in the winning combinations for comparison later.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,16 @@
 require_relative "board.rb"
 
 class Game
+  @@winning_combos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 4, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
   attr_accessor :game_board
 
   def initialize(player1, player2)
