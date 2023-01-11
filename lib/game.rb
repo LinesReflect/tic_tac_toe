dcb1a43f88ad11d2e80383789c2f61567232ce4a
Reflection <reflectionoflines@gmail.com>
2023-01-11 13:50:41 -0600

    Add method to compare values and see if a player has won.

--- lib/game.rb
+++ lib/game.rb
@@ -37,7 +37,28 @@ class Game
   end
 
   def check_status
-    switch_turns
-    choose_placement
+    if player_won?
+      puts "#{@@player_turn.name} is the winner!"
+    else
+      switch_turns
+      choose_placement
+    end
+  end
+
+  def player_won?
+    @@winning_combos.each do |combo|
+      placement_index_1 = combo[0]
+      placement_index_2 = combo[1]
+      placement_index_3 = combo[2]
+
+      spot_1 = @game_board.spots[placement_index_1]
+      spot_2 = @game_board.spots[placement_index_2]
+      spot_3 = @game_board.spots[placement_index_3]
+
+      if spot_1 == spot_2 && spot_2 == spot_3
+        return true
+      end
+    end
+    return false
   end
 end
