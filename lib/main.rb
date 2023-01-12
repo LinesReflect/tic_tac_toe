3df39551080af37155b374ce1d17331a7899d094
Reflection <reflectionoflines@gmail.com>
2023-01-12 14:33:43 -0600

    Create a method that asks the player if they want to start another game.

--- lib/main.rb
+++ lib/main.rb
@@ -2,6 +2,22 @@ require_relative "board.rb"
 require_relative "game.rb"
 require_relative "user.rb"
 
-user_1 = User.new("player 1")
-user_2 = User.new("player 2")
-Game.new(user_1, user_2).begin_game
+def create_game
+  user_1 = User.new("player 1")
+  user_2 = User.new("player 2")
+  Game.new(user_1, user_2).begin_game
+  another_game
+end
+
+def another_game
+  puts "Would you like to play again? Y/N"
+  answer = gets.chomp
+  if answer == "Y" || answer == "N"
+    puts answer == "Y" ? create_game : "Thanks for playing!"
+  else
+    puts "Please answer with 'Y' or 'N'"
+    another_game
+  end
+end
+
+create_game
