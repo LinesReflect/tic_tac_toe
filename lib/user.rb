75e17b091b260bf64dfdc261e0677c7aa1f7627e
Reflection <reflectionoflines@gmail.com>
2023-04-26 10:31:54 -0500

    Refactor code.

--- lib/user.rb
+++ lib/user.rb
@@ -3,24 +3,24 @@ require_relative "board.rb"
 class User
   attr_accessor :player, :name, :game_piece
 
-  def initialize(player, name = set_name(player), game_piece = set_game_piece(player))
+  def initialize(player, name = player_name(player), game_piece = player_game_piece(player))
     @player = player
     @name = name
-    @game_piece = game_piece 
+    @game_piece = game_piece
   end
 
-  def set_name(player)
+  def player_name(player)
     puts "Hello #{player}, please enter your name." 
-    return self.name = gets.chomp
+    gets.chomp
   end
 
-  def set_game_piece(player)
+  def player_game_piece(player)
     if player == "player 1"
       puts "Okay #{@name}, you will use X's and go first."
-      return "X"
+      'X'
     else
       puts "#{@name}, you will move second using O's."
-      return "O"
+      'O'
     end
   end
 end
